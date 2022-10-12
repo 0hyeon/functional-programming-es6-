080533da6911bd2ea5d15dfe27bd18594d8fbdf1
0hyeon <djdjdjk2006@naver.com>
2022-10-13 00:29:12 +0900

    리스트순회

---
+++ 1.기존과 달라진 ES6에서의 리스트 순회.js
@@ -0,0 +1,14 @@
+const list = [1, 2, 3];
+for (var i = 0; i < list.length; i++) {
+  console.log(list[i]);
+}
+const str = "abc";
+for (var i = 0; i < str.length; i++) {
+  console.log(str[i]);
+}
+for (const a of list) {
+  console.log(a);
+}
+for (const a of str) {
+  console.log(a);
+}
