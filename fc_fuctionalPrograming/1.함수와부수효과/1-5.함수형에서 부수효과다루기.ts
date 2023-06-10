28b6a04b4c95bb604104a05403f1ecfcd560d771
0hyeon <djdjdjk2006@naver.com>
2023-06-10 11:13:47 +0900

    fix: 2-3.계산과부수효과의분리

--- fc_fuctionalPrograming/1.함수와부수효과/1-5.함수형에서 부수효과다루기.ts
+++ fc_fuctionalPrograming/1.함수와부수효과/1-5.함수형에서 부수효과다루기.ts
@@ -1,7 +1,7 @@
 //순수함수만 사용하는 이유는 합성을 쉽게하기위해
 //프로시저나 서브루틴 과는 달리 부수효과가 포함되지 않는다.
 
-//합성하기 어려운 부수효과 for문
+//합성하기 어려운 부수효과 for문(합성하기 어려움)
 function sum() {
   let sum = 0;
   for (let i = 0; i <= 100; i++) {
@@ -10,7 +10,7 @@ function sum() {
   return sum;
 }
 console.log(sum());
-
+//재귀를 통한 함수형프로그래밍 (순수함수 하지만 장황)
 function sum_1_to_100() {
   function go(sum, i) {
     if (i > 100) {
@@ -21,3 +21,15 @@ function sum_1_to_100() {
   return go(0, 1);
 }
 console.log(sum_1_to_100());
+//매번 달라지지 않을만한 코드를 리팩토링 (일반화된 방식으로 추상화)
+function loop(fn, acc, list) {
+  if (list.length === 0) return acc;
+  const [head, ...tail] = list;
+  return loop(fn, fn(acc, head), tail);
+}
+//범위
+const range = (start, end) =>
+  Array.from({ length: end - start - 1 }, (_, index) => index + start);
+//무엇을할지
+const plus = (a, b) => a + b;
+console.log(loop(plus, 0, range(1, 100))); //5050
