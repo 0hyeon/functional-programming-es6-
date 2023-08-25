438870ec03dea7da1f4af8b7c5e2a7fabb5f3887
0hyeon <djdjdjk2006@naver.com>
2023-08-25 13:56:32 +0900

    feat:if문살펴보기

--- fc_fuctionalPrograming/3.여러개일수있는값/3-9.2중for문.ts
+++ fc_fuctionalPrograming/3.여러개일수있는값/3-9.2중for문.ts
@@ -26,12 +26,13 @@ for (const suit of suits) {
 //모든 카드 목록은 아래의 작업이 완료
 const cards2 =
   //아래의 작업을 모든 무늬에 적용
-  suits.map((suit) =>
+  suits.flatMap((suit) =>
     //아래의 작업을 모든숫자에 적용
     numbers.map(
       (number) =>
         //카드는 무늬와 숫자를 연결 한 문자열
         suit + number
+        //무늬별로 나누어진 카드를 하나로 합치기
     )
   );
 
