6d1faf6c06843371959cb2754eeb6f4321ca2736
0hyeon <djdjdjk2006@naver.com>
2023-09-25 13:46:18 +0900

    feat: 커링이 적용된 함수

--- fc_fuctionalPrograming/5.여러모양의함수와map/5-4.Curryinig.ts
+++ fc_fuctionalPrograming/5.여러모양의함수와map/5-4.Curryinig.ts
@@ -1,11 +1,12 @@
 // 커링(Currying):
-//인자가 여러개인 함수를
-//인자가 하나인 함수들의
-//함수열(sequence of functions)로 만들기
+//인자가 여러개인 함수를 인자가 하나인 함수들의 함수열(sequence of functions)로 만들기
 //이러한 함수를 curried function
-
+//ex) (a:A)=>(b:B)=>(c:C)=>D
 // const delevery = (present: string) => (from: string) => (to: string) => {
 
+//인자가 여러개인 함수: (A,B,C)=>D
+//커링을 적용 curry((A,B,C)=>D)
+//(커링이된함수)curried function : (A)=>(B)=>(C)=>D
 const delevery = (present: string, from: string, to: string) => {
   return `
     보내는 물건 : ${present}
@@ -15,11 +16,17 @@ const delevery = (present: string, from: string, to: string) => {
 };
 const curry3 =
   <A, B, C, D>(f: (a: A, b: B, c: C) => D) =>
+  //개별적인 함수를 개별적으로
   (a: A) =>
   (b: B) =>
   (c: C) =>
     f(a, b, c);
 
+//이제 위 커링함수로 딜리버링을 커버해보자
+const curriedDelivery = curry3(delevery);
+
 export const main = () => {
   console.clear();
+  const momsPresend = curriedDelivery("cultureLand")("mom");
+  console.log(momsPresend("son"));
 };
